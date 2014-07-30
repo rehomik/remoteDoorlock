2f03287660cf9273332ed9974c9e95933dd21656
rehomik <re.ho.mik@gmail.com>
2014-07-30 16:57:59 +0900

    기능 추가 - 키패드 입력에 따른 입력 피드백 완료.

--- gate_server/public/javascripts/index.js
+++ gate_server/public/javascripts/index.js
@@ -3,11 +3,49 @@
  * Written by rehomik
  */
 
+var _typedKeyPadArray = [];
+
+var typeKeypad = function (key_pad_num, input_complete_callback) {
+
+  _typedKeyPadArray.push(key_pad_num);
+
+  if (4 === _typedKeyPadArray.length) {
+
+    input_complete_callback(true);
+
+    return;
+  }
+
+  input_complete_callback(false);
+
+}
+
+var refreshKeypadColor = function (data_array) {
+
+  var data_length = data_array.length;
+
+  var typed_pad_id = "#typedPad" + data_length;
+
+  $(typed_pad_id).removeClass("btn-danger").addClass("btn-success").text('O');
+}
+
 $('#pad1, #pad2, #pad3, #pad4, #pad5, #pad6, #pad7, #pad8, #pad9').each(function addClick() {
 
   $(this).click(function onClick() {
 
-    alert($(this).text());
+    var key_pad_num = $(this).text();
 
+    typeKeypad(key_pad_num, function inputComplete(result) {
+
+      refreshKeypadColor(_typedKeyPadArray);
+
+      if (result) {
+
+        console.log("confirm");
+
+        return;
+      }
+    });
   });
+
 });
